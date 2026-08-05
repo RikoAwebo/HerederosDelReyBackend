5d1eac319684660d52e3875a6a6721dc9af2de26
keko29k <mepicanloscoocos16@gmail.com>
2026-08-05 12:48:48 -0400

    caja

--- HerederosDelReyBackend/HerederosDelReyBackend/DTOs/DTO_UPDATE/CajaUpdateDto.cs
+++ HerederosDelReyBackend/HerederosDelReyBackend/DTOs/DTO_UPDATE/CajaUpdateDto.cs
@@ -2,18 +2,9 @@
 {
     public class CajaUpdateDto
     {
-        public decimal? MontoInicial { get; set; }
+        
+        public string EstadoCaja { get; set; } = null!;
 
-        public decimal? MontoFinal { get; set; }
-
-        public DateTime? FechaApertura { get; set; }
-
-        public DateTime? FechaCierre { get; set; }
-
-        public decimal? Gastos { get; set; }
-
-        public string? EstadoCaja { get; set; }
-
-        public int? UsuarioId { get; set; }
+        public string? Observacion { get; set; }
     }
 }
